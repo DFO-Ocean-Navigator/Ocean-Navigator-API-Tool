be01a47ba5314d0d96e14afb6c3b55c61ce313f0
Noah Gallant <noah.gallant@mun.ca>
2018-06-05 14:46:13 -0230

    Added API Documentation

---
+++ navtreedata.js
@@ -0,0 +1,38 @@
+var NAVTREE =
+[
+  [ "Ocean Navigator", "index.html", [
+    [ "Ocean Navigator", "md__home_noah_Ocean-Data-Map-Project_README.html", null ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d6/d42/colormap_8py.html#a2c3a0068201cf56af6f5bd838c4926e9",
+"db/d1e/views_8py.html#a825a0c15bd0e1636127ae583ba87034a",
+"functions.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
