3a5eea48b1a938fbb44240bddaa72b15db456120
Noah Gallant <noah.gallant@mun.ca>
2018-07-12 11:57:14 -0230

    Added Things

--- js/mainTabs.js
+++ js/mainTabs.js
@@ -1,4 +1,5 @@
-
+var active_dataset
+var panesPopulated = false;
 
 function openTab(evt, name) {
 
@@ -31,7 +32,7 @@ function openContents(event, name, all, button) {
     document.getElementById(button).className = 'howto_button'
     document.getElementById(name).style.display = "none"
     document.getElementById(all).style.backgroundColor = "rgb(245, 245, 245)";
-    document.getElementById(button).style.backgroundColor = "initial";
+    document.getElementById(button).style.backgroundColor = "block";
     //document.getElementById(button).style.hover.backgroundColor = "rgb(204, 204, 204)"
   } else {
 
@@ -56,136 +57,198 @@ function openContents(event, name, all, button) {
 
 }
 
+function updateSelection(datasetid) {
+  nextDiv = datasetid + "_pane_div"
+  console.warn("DATASET ID:")
+  console.warn(datasetid)
+  console.warn("ACTIVE DATASET:")
+  console.warn(active_dataset)
+  if (active_dataset === undefined) { //No other dataset is being displaying
+    active_dataset = datasetid
+    document.getElementById(nextDiv).style.display = 'inline-flex';
+    document.getElementById(datasetid + "_dataset_button").className = 'ui-state-active'
+    document.getElementById(nextDiv).style.backgroundColor = "rgb(204,204,204)"
+  } else if (active_dataset === datasetid) {  //The selected dataset is already being displayed
+    
+    document.getElementById(nextDiv).style.display = 'none';
+    document.getElementById(datasetid + '_dataset_button').className = ''
+    document.getElementById(nextDiv).style.backgroundColor = "rgb(255,255,255)";
+    
+    active_dataset = undefined
+
+  } else {  // A different dataset from the one being selected is displayed
+    
+    previousDataset = active_dataset + "_pane_div"
+    document.getElementById(active_dataset + '_dataset_button').className = '';
+    
+    document.getElementById(previousDataset).style.display = 'none';  //Close previous dataset
+    document.getElementById(nextDiv).style.display = 'inline-flex';         //Open new dataset
+    document.getElementById(datasetid + '_dataset_button').className = 'ui-state-active'; //Arrow animation
+    document.getElementById(nextDiv).style.backgroundColor = "rgb(204,204,204)";
+
+    active_dataset = datasetid; //Update current dataset being displayed
+  
+  }
+
+  console.warn(active_dataset)
+}
+
+
+//CREATES TABLE OF AVAILABLE DATASETS
+//ALSO CREATES THE GENERAL STRUCTURE OF THE PANES
 function getDatasets() {
-  let dataset_info = '';
+
   $.ajax({
-    url: "http://navigator.oceansdata.ca/api/datasets/?id", success: function(result){
-      dataset_info = result;
-      console.warn(dataset_info);
+    url: "http://navigator.oceansdata.ca/navigator/api/datasets/?id", success: function(datasets){
+
+      size = datasets.length;
+      rows = 5 - (size % 5);    //Puts 5 datasets per row
+      rows = (size + rows) / 5  //Determines number of rows required for datasets
+      
+      let createButtons = "";
+
       row = 0;
-      dataset_buttons = '<table style="width: 100%"><h4>Dataset  (id)<h4>';
-      size = dataset_info.length;
-      console.warn(size)
-      col = 0
-      for (let i in result) {
-        current_dataset = dataset_info[i]
+      cell = 0;
+      paneCell = 0;
 
-        if (col == 0) {
-          dataset_buttons += '<tr id=' + "'rowNumber" + row + "' " + 'style="width: 100%; height: 40px">'
+      //Controls the ROWS
+      while (row < rows) {
+
+        //Create row
+        createButtons += "<tr class='dataset_buttons_table_row' id='row" + row + "_dataset_buttons_table_row' style='width=20%'>"
+        col = 1 // Initialize(reset) col #
+
+        //Controls the CELLS
+        while (col <=5 && datasets[cell] !== undefined) { //Runs to the end of the row, or until there are no more datasets
           
+          createButtons += "<td class='dataset_buttons_table_col' id='" + datasets[cell]['id'] + "_dataset_buttons_table_col' style='width: 20%; height:100%'>"
+
+          createButtons += "<button id='" + datasets[cell]['id'] + "_dataset_button' " + "onclick=" +"'updateSelection(" + '"' + datasets[cell]['id'] + '")' + "'" + ' style="text-align: left; width: 100%; height: 60px"' + ">"
+          createButtons += "<i class='fa fa-angle-down ' style='font-size: 20px;'></i>"
+          createButtons += "<h4 style='display: inline; padding: 20px'>" + datasets[cell]['value'] + " (" + datasets[cell]['id'] + ")</h4>"
+          createButtons += "</button>"
+
+          createButtons += "</td>"
+          col += 1;
+          cell += 1;
         }
+        createButtons += "</tr>"
+        createButtons += "<tr class='information_pane_row' id='row" + row + "_pane_row'>"
+        createButtons += "<td class='information_pane_col' id='row" + row + "_pane_col' colspan='5'>"
+        col = 1
+        while (col <= 5 && datasets[paneCell] !== undefined) {
+          createButtons += "<div class='row" + row + "_pane_div' id='" + datasets[paneCell]['id'] + "_pane_div' style='display:none; padding: 20px; width: 100%'>"
+          createButtons += "</div>"  //This is where the data will be populated
 
-        dataset_buttons += '<td style="width: 20%; height: 40px; padding: 1px">'
-        button_content = current_dataset['value'] + " (" + current_dataset['id'] + ")";
-        dataset_buttons += '<button id="' + current_dataset['id'] + '" onclick="openDataset(' + "'" + current_dataset['id'] + "', 'row" + row + "'" + ')" style="width: 100%; height: 100%; text-align: left; background-color: rgb(240, 240, 240)">' + button_content + '</button>'
-        dataset_buttons += '</td>'
-        console.warn("COL: ")
-        console.warn(col)
-
-        if (col == 4) {
-          dataset_buttons += '</tr>'
-          dataset_buttons += "<tr id='row" + row + "'" + "style='width: 100%;'>"
-          dataset_buttons += "</tr>"
-
-          col = -1
-          row += 1
+          col += 1
+          paneCell += 1;
         }
-        
-        
-        console.warn(current_dataset['value']);
-
-        col += 1
+        createButtons += "</td></tr>"
+        row += 1
       }
-      dataset_buttons += '</table>'
-      console.warn(dataset_buttons)
+      console.warn(createButtons)
+      document.getElementById('dataset_buttons_table').innerHTML = createButtons
 
-      document.getElementById('load_datasets').innerHTML = dataset_buttons;
-      
-      
+      populateVariables(datasets);
+      populateTimestamps(datasets)
+      //populateDepths(datasets);
     }
-  });
-}
-
-function openDataset(dataset_id, rowClass) {
-
-  //Button not active
-  if (document.getElementById(dataset_id).className !== "datasetactive") {
-    if (document.querySelector('.datasetactive') !== null) {
-      elements = document.getElementsByClassName('datasetactive')
-      console.warn("LENGTH: ")
-      console.warn(elements.length)
-      for (var i = elements.length - 1, item; item = elements[i]; i--) {
-        elements[i].className = ""
-      }
-      
-    }
-    document.getElementById(dataset_id).className = "datasetactive"
-    document.getElementById(rowClass).style.display = 'table-row'
-  
-  //Button Active
-  } else {
-    document.getElementById(rowClass).style.display = 'none'
-    document.getElementById(dataset_id).className = ""
-  }
-  
-  dataset_values = "<td class='inputvalues_tab' colspan='5'>"
-  
-  dataset_values += "HELLO WORLD"
-  dataset_values += populateVariables(dataset_id)
-  
-  dataset_values += "</td>"
-
-  
-  if (document.querySelector('.inputvalues_tab') !== null) {
-    elements = document.getElementsByClassName('inputvalues_tab')
-    for (var i = elements.length - 1, item; item = elements[i]; i--) {
-      elements[i].style.display = 'none';
-    }
-  }
-  document.getElementById(rowClass).innerHTML = dataset_values
-
+  })
+  createTable = "<table class='dataset_buttons_table' id='dataset_buttons_table'></table>"
+  document.getElementById('load_datasets').innerHTML = createTable
 
 }
 
-function populateVariables(dataset_id) {
-  console.warn(dataset_id)
+//ADDS DATA TO EMPTY TABLE OF AVAILABLE VARIABLES
+function populateVariables(datasets) {
 
-  i = "http://localhost:5000/api/variables/?dataset=" + dataset_id
+  num_datasets = datasets.length
+  num_datasets = Array.apply(null, Array(num_datasets));
+  num_datasets = num_datasets.map(function (x, i) {return i})
+  
+  $(num_datasets).each(function() {
+    var number = this;
+    $.ajax({
+      url: "http://navigator.oceansdata.ca/api/v1.0/variables/?dataset=" + datasets[number]['id'], success: function(variables) {
+        
+        variableTable = "<table style='border: 1px solid black; padding: 5px'>"
+        variableTable += "<tr class=variable_table_row'>";
+        variableTable += "<td class='variable_value_table_col' style='font-size: 19px'>Variable</td>";
+        variableTable += "<td class='variable_id_table_col' style='font-size: 19px'>ID</td>";
+        variableTable += "</tr>"
+        for (variable in variables) {
+          variableTable += "<tr class='variable_table_row' id='" + variables[variable]['id'] + "_" + datasets[number]['id'] + "_variable_table_row'>"
+          variableTable += "<td class='variable_value_table_col' id='" + variables[variable]['id'] + "_" + datasets[number]['id'] + "_variable_value_table_col'>" + variables[variable]['value'] + "</td>"
+          variableTable += "<td class='variable_id_table_col' id='" + variables[variable]['id'] + "_" + datasets[number]['id'] + "_variable_id_table_col'>" + variables[variable]['id'] + "</td>"
+          variableTable += "</tr>"
+        }
+        variableTable += "</table>"
 
-
-  //httpRequest = new XMLHttpRequest();
-  //httpRequest.open('GET', i, true)
-  //httpRequest.send(null)
-  console.warn(i)
-  $.ajax({
-    url: i, success: function(result){
-      dataset_info = result;
-      console.warn(dataset_info);
-      
-      variableTable = "<table>"
-
-      //CREATE TITLE
-      variableTable += '<tr><h3>Variables</h3></tr>'
-
-      for (let i in result) {
-    
-        variableTable += '<tr>'
-    
-    
-        variableTable += '</tr>'
-    
+        requiredDiv = datasets[number]['id'] + "_pane_div"
+        document.getElementById(requiredDiv).innerHTML = variableTable
       }
-    
-      variableTable += "</table>"
-      console.warn("IN AJAX")
-       
-      //Adds HTML to webpage
-      //document.getElementById('load_datasets').innerHTML = dataset_buttons;
-      
-      
-    },
-  });
-  console.warn("After AJAX")
+    })
+  })
+}
+
+//ADDS DATA TO EMPTY TABLE OF AVAILABLE TIMESTAMPS
+function populateTimestamps(datasets) {
+
+  num_datasets = datasets.length
+  num_datasets = Array.apply(null, Array(num_datasets));
+  num_datasets = num_datasets.map(function (x, i) {return i})
   
+  $(num_datasets).each(function() {
+    var number = this;
+    $.ajax({
+      url: "http://navigator.oceansdata.ca/api/v1.0/timestamps/?dataset=" + datasets[number]['id'], success: function(timestamps) {
+        
+      variableTable = "<table style='border: 1px solid black; padding: 5px;'>"
+        variableTable += "<tr class=variable_table_row'>";
+        variableTable += "<td class='timestamp_value_table_col'></td>"
+        variableTable += "<td class='variable_value_table_col' style='font-size: 19px'>Time</td>";
+        variableTable += "<td class='variable_id_table_col' style='font-size: 19px'>Index</td>";
+        variableTable += "</tr>"
+        
+        variableTable += "<tr class='variable_table_row' id='" + timestamps[0]['id'] + "_" + datasets[number]['id'] + "_variable_table_row'>"
+        variableTable += "<td class='timestamp_table_col' id='first_timestamp_value_table_col'>" + 'First' + "</td>"
+        variableTable += "<td class='timestamp_table_col' id='" + timestamps[0]['id'] + "_" + datasets[number]['id'] + "_variable_id_table_col'>" + timestamps[0]['value'] + "</td>"
+        variableTable += "<td class='timestamp_index_table_col'>" + timestamps[0]['id'] + "</td>"
+        variableTable += "</tr>"
+        
+        variableTable += "<tr class='variable_table_row' id='" + timestamps[0]['id'] + "_" + datasets[number]['id'] + "_variable_table_row'>"
+        variableTable += "<td class='timestamp_table_col' id='first_timestamp_value_table_col'>" + 'Last' + "</td>"
+        variableTable += "<td class='timestamp_table_col' id='" + timestamps[0]['id'] + "_" + datasets[number]['id'] + "_variable_id_table_col'>" + timestamps[timestamps.length - 1]['value'] + "</td>"
+        variableTable += "<td class='timestamp_index_table_col'>" + timestamps[timestamps.length - 1]['id'] + "</td>"
+        variableTable += "</tr>"
+
+        variableTable += "</table>"
+
+        requiredDiv = datasets[number]['id'] + "_pane_div"
+        document.getElementById(requiredDiv).innerHTML += variableTable
+      
+      }
+    })
+  })
+
+
+}
+
+//ADDS DATA TO EMPTY TABLE OF AVAILABLE DEPTHS
+function populateDepths(datasets) {
+  /*
+  num_datasets = datasets.length
+  num_datasets = Array.apply(null, Array(num_datasets));
+  num_datasets = num_datasets.map(function (x, i) {return i})
   
+  $(num_datasets).each(function() {
+    var number = this;
+    $.ajax({
+      url: "http://trinity:50/navigator/api//?dataset=" + datasets[number]['id'], success: function(variables) {
+        console.warn("VARIABLES: ")
+        console.warn(variables)
+      }
+    })
+  })
+  */
 }
