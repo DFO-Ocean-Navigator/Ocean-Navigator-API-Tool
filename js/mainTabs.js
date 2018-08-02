e36bc0d8864e8ae08af45282fe614957b4d039e0
Noah Gallant <noah.gallant@mun.ca>
2018-08-02 17:46:15 -0230

    Fixed Table issue from last commit

--- js/mainTabs.js
+++ js/mainTabs.js
@@ -133,7 +133,7 @@ function getDatasets() {
 
         //Controls the CELLS
         while (col <=3 && datasets[cell] !== undefined) { //Runs to the end of the row, or until there are no more datasets
-          
+          console.warn(datasets[cell]['id'])
           createButtons += "<td class='dataset_buttons_table_col' id='" + datasets[cell]['id'] + "_dataset_buttons_table_col' style='width: 33.33333%; height:100%'>"
 
           createButtons += "<button id='" + datasets[cell]['id'] + "_dataset_button' " + "onclick=" +"'updateSelection(" + '"' + datasets[cell]['id'] + '")' + "'" + ' style="text-align: left; width: 100%; height: 60px"' + ">"
@@ -145,21 +145,28 @@ function getDatasets() {
           col += 1;
           cell += 1;
         }
+
         createButtons += "</tr>"
         createButtons += "<tr class='information_pane_row' id='row" + row + "_pane_row'>"
         createButtons += "<td class='information_pane_col' id='row" + row + "_pane_col' colspan='5'>"
         col = 1
+
+        // Initializes divs for dataset information (Variables | Timestamps | Depths)
         while (col <= 3 && datasets[paneCell] !== undefined) {
           createButtons += "<div class='row" + row + "_pane_div' id='" + datasets[paneCell]['id'] + "_pane_div' style='display:none; padding: 20px; width: 100%'>"
-          createButtons += 
+          createButtons += '<table id=' + datasets[paneCell]['id'] + '_table_pane_div" style="width:100%;">'
+          createButtons += '<tr id=' + datasets[paneCell]['id'] + 'row_table_pane_div" style="width: 100%;">'
           
-          createButtons += "<table id=" + '"` + datasets[paneCell][' + "'id'] +" + ' `_table_pane_div" style="width:100%;">'
-          createButtons += '<tr id="` + datasets[paneCell]' + "['id']" + ' + `row_table_pane_div" style="width: 100%;">'
-          createButtons += '<td class="pane_cell" id="` + datasets[paneCell]' + "['id'] + `_variables_pane_div" + '" style="vertical-align: text-top; border-right: 4px solid white;">'
+          // Variables
+          createButtons += '<td class="pane_cell" id="' + datasets[paneCell]['id'] + "_variables_pane_div" + '" style="vertical-align: text-top; border-right: 4px solid white;">'
           createButtons += '</td>'
-          createButtons += '<td class="pane_cell" id="`' + " + datasets[paneCell]['id']" + ' + `_timestamps_pane_div" style="vertical-align: text-top;">'
+          
+          // Timestamps
+          createButtons += '<td class="pane_cell" id="' + datasets[paneCell]['id'] + '_timestamps_pane_div" style="vertical-align: text-top;">'
           createButtons += '</td>'
-          createButtons += '<td class="pane_cell" id="`' + " + datasets[paneCell]['id']" + ' + `_depths_pane_div" style="border-left: 4px solid white; vertical-align: text-top;">'
+          
+          // Depths
+          createButtons += '<td class="pane_cell" id="' + datasets[paneCell]['id'] + '_depths_pane_div" style="border-left: 4px solid white; vertical-align: text-top;">'
           createButtons += '</td>'
           createButtons += '</tr>'
           createButtons += '</table>'
@@ -198,11 +205,16 @@ function populateVariables(datasets) {
     $.ajax({
       url: "http://trinity:50/navigator/api/v1.0/variables/?dataset=" + datasets[number]['id'], success: function(variables) {
         
+        // Initializes Variable Table
         variableTable = "<table style='width: 100%; padding: 5px'>"
-        variableTable += "<tr class=variable_table_row'>";
+        
+        // Sets Table title (Variable | ID)
+        variableTable += "<tr class=variable_table_row' style='display: flex;'>";
         variableTable += "<td class='variable_value_table_col' style='font-size: 19px'>Variable</td>";
         variableTable += "<td class='variable_id_table_col' style='font-size: 19px'>ID</td>";
         variableTable += "</tr>"
+        
+        // Adds Variable Name | ID Pair to each row in the table
         for (variable in variables) {
           variableTable += "<tr class='variable_table_row' id='" + variables[variable]['id'] + "_" + datasets[number]['id'] + "_variable_table_row'>"
           variableTable += "<td class='variable_value_table_col' id='" + variables[variable]['id'] + "_" + datasets[number]['id'] + "_variable_value_table_col'>" + variables[variable]['value'] + "</td>"
@@ -210,11 +222,14 @@ function populateVariables(datasets) {
           variableTable += "</tr>"
         }
         variableTable += "</table>"
-
+        
+        // Places the Variable Table in the appropriate place
         requiredDiv = datasets[number]['id'] + "_variables_pane_div"
         document.getElementById(requiredDiv).innerHTML = variableTable
       },
       error: function() {
+
+        // Displays an error message instead of the variable table
         variableTable = "<h4 class='error'>Variables Failed to Load</h4>"
         requiredDiv = datasets[number]['id'] + "_variables_pane_div"
         document.getElementById(requiredDiv).innerHTML = variableTable
@@ -238,10 +253,9 @@ function populateTimestamps(datasets) {
       url: "http://trinity:50/navigator/api/v1.0/timestamps/?dataset=" + datasets[number]['id'], success: function(timestamps) {
         
       variableTable = "<table style='height: 100%; width: 100%; padding: 5px;'>"
-        variableTable += "<tr class=variable_table_row'>";
-        variableTable += "<td class='timestamp_value_table_col'></td>"
-        variableTable += "<td class='variable_value_table_col' style='font-size: 19px'>Time</td>";
-        variableTable += "<td class='variable_id_table_col' style='font-size: 19px'>Index</td>";
+        variableTable += "<tr class=variable_table_row' style='display: flex;'>";
+        variableTable += "<td class='timestamp_value_table_col' style='font-size: 19px; text-align: left; '>Time</td>";
+        variableTable += "<td class='timestamp_id_table_col' style='font-size: 19px'>Index</td>";
         variableTable += "</tr>"
         
         variableTable += "<tr class='variable_table_row' id='" + timestamps[0]['id'] + "_" + datasets[number]['id'] + "_variable_table_row'>"
@@ -288,45 +302,50 @@ function constructDepthField(datasets) {
         minDepth = depths[1]['value'];
         maxDepth = depths[depths.length - 1]['value'];
 
-        contents = 
-        '\
-        <div style="font-size: 19px; text-align: left;">Depth</h3>\
-        <div style="padding-bottom: 20px; text-align: justify; ">In each dataset, data can be found at various depth layers. This tool allows you to provide your desired depth and will return the depth of the nearest available layer, and the associated index to be used in the navigators API </div>\
-          <table style="width:100%;">\
-            <tr>\
-        <td class="depth_cells_left">Min Depth</td>\
-        '
-        contents += '<td class="depth_cells">` + minDepth + `</td>'
-        contents += '</tr>\
-            <tr style="border-bottom: 2px solid white">\
-            <td class="depth_cells_left">Max Depth</td>\
-        '  
-        contents += '<td class="depth_cells">' + maxDepth + '</td>\
-            </tr>\
-            <tr>\
-              <td colspan="2" style="padding-top: 20px; padding-bottom: 10px;">\
-                <table style="width:100%;">\
-                  <tr>\
-                  <td class="depth_cells_left">Desired Depth (m)</td>\
-            '
-            contents += '<td style="text-align: right;"><input type="text" name="desiredDepth" id="' + datasets[number]['id'] + '_desiredDepthValue" style="width: 132px;"></br></td>\
-                  </tr>\
-                  <tr>' 
-            contents += '<td class="depth_search" colspan="2"><button onclick="' + "search('" + datasets[number]['id'] + "'" + ')" style="background-color: rgb(175,175,175)">Search</button></td>\
-                  </tr>\
-                </table>\
-              </td>\
-            </tr>\
-            <tr style="padding-top: 10px;">\
-              <td class="depth_cells_left">Nearest Index</td>'
-            contents += '<td class="depth_cells_output" id="' + datasets[number]['id'] + '_index_depths_pane_div"></td>\
-            </tr>\
-            <tr>\
-              <td class="depth_cells_left">Nearest Depth</td>'
-            contents += '<td class="depth_cells_output" id="' + datasets[number]['id'] + '_result_depths_pane_div"></td>\
-            </tr>\
-          </table>\
-        '
+        contents =  '<div style="font-size: 19px; text-align: left;">Depth</h3>'
+        contents += '<div style="padding-bottom: 20px; text-align: justify; ">In each dataset, data can be found at various depth layers. This tool allows you to provide your desired depth and will return the depth of the nearest available layer, and the associated index to be used in the navigators API </div>'
+        contents += '<table style="width:100%;">'
+
+        // Sets Min Depth
+        contents += '<tr class="depth_table_row">'
+        contents += '<td class="depth_cells_left">Min Depth</td>'
+        contents += '<td class="depth_cells">' + minDepth + '</td>'
+        contents += '</tr>'
+
+        // Sets Max Depth
+        contents += '<tr class="depth_table_row" style="border-bottom: 2px solid white">'
+        contents += '<td class="depth_cells_left">Max Depth</td>'
+        contents += '<td class="depth_cells">' + maxDepth + '</td>'
+        contents += '</tr>'
+        contents += '<tr>'
+        contents += '<td colspan="2" style="padding-top: 20px; padding-bottom: 10px;">'
+        contents += '<table style="width:100%;">'
+        
+        // User Input depth field
+        contents += '<tr class="depth_table_row">'
+        contents += '<td class="depth_cells_left">Desired Depth (m)</td>'
+        contents += '<td style="text-align: right;"><input type="text" name="desiredDepth" id="' + datasets[number]['id'] + '_desiredDepthValue"></td>'
+        contents += '</tr>'
+
+        // Search Button
+        contents += '<tr class="depth_table_row" style="text-align: right;">' 
+        contents += '<td><button onclick="' + "search('" + datasets[number]['id'] + "'" + ')" style="background-color: rgb(175,175,175); width: 50%;">Search</button></td>'
+        contents += '</tr>'
+        contents += '</table>'
+        contents += '</td>'
+        contents += '</tr>'
+
+        // Nearest depth(s) to user input
+        contents += '<tr class="depth_table_row" style="padding-top: 10px;">'
+        contents += '<td class="depth_cells_left">Nearest Index</td>'
+        contents += '<td class="depth_cells_output" id="' + datasets[number]['id'] + '_index_depths_pane_div"></td>'
+        contents += '</tr>'
+        contents += '<tr class="depth_table_row">'
+        contents += '<td class="depth_cells_left">Nearest Depth</td>'
+        contents += '<td class="depth_cells_output" id="' + datasets[number]['id'] + '_result_depths_pane_div"></td>'
+        contents += '</tr>'
+        contents += '</table>'
+        
 
         requiredDiv = datasets[number]['id'] + "_depths_pane_div"
         document.getElementById(requiredDiv).innerHTML += contents;
